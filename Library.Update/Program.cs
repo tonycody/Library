01481e715dfb610f8f3e317236983f737cfac39a
Albert Lindsay <>
2012-02-26 18:58:57 +0900

    Library
    Library.Update追加

---
+++ Library.Update/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace Library.Update
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length != 4) return;
+
+            try
+            {
+                if (args[0] != "")
+                {
+                    try
+                    {
+                        Process process = Process.GetProcessById(int.Parse(args[0]));
+                        process.WaitForExit();
+                    }
+                    catch
+                    {
+
+                    }
+                }
+
+                if (Directory.Exists(args[2]))
+                    Directory.Delete(args[2], true);
+
+                Directory.Move(args[1], args[2]);
+
+                if (args[3] != "")
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.FileName = args[3];
+                    startInfo.WorkingDirectory = Path.GetDirectoryName(args[3]);
+
+                    Process.Start(startInfo);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(string.Join("\r\n", args), "Library.Update");
+                MessageBox.Show(e.Message, "Library.Update");
+            }
+        }
+    }
+}
---
+++ Library.Update/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Library.Update")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Library.Update")]
+[assembly: AssemblyCopyright("Copyright © Albert Lindsay 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("485786c0-4db9-460f-8ff7-53dccc13e950")]
+[assembly: AssemblyVersion("0.0.0.13")] // pgrTHAiAzuJHatwRgEggZEWBBBCLpDsD+X1xwwZA3DhHjD5Ox0tVCEIvN1O4AQ/H+jPlIfJNSmYAQkyoS3V+Gw==
