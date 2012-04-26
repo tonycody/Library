1afe5fd041f0833405d3e7872628ad4cc5d825d7
Albert Lindsay <>
2012-04-26 15:13:25 +0900

    Library

--- Library.Net.Amoeba/Properties/AssemblyInfo.cs
+++ Library.Net.Amoeba/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8c62c763-25dc-4cf4-93fb-96b406eaf9cc")]
-[assembly: AssemblyVersion("0.0.0.1611")] // h0wlYoduWYnMjmggtbRgbzg8cB39sYAJmriTfdUoVr4KTWsKbIyACsUpwEhKlWS5kz5EtoM11vBBZadvAxUjBQ==
+[assembly: AssemblyVersion("0.0.0.1612")] // +WZ4zogUuw25oEHSvLgeDtj88BE5wp5SIJh8rSEO5SvlnH39BVA8GSicFPF1uxYgenqYiTj5If7cI9KbC+RLFw==
 
 #if DEBUG
 [assembly: InternalsVisibleTo("Library.UnitTest")]
