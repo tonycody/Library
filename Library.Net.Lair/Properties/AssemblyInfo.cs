1897f2fa2976a91db86a3582080fe0237c5a9d3e
Lyrise <>
2013-01-01 01:24:47 +0900

    Library
    著作権周りの表記の変更

--- Library.Net.Lair/Properties/AssemblyInfo.cs
+++ Library.Net.Lair/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Library.Net.Lair")]
-[assembly: AssemblyCopyright("Copyright © Lyrise 2009-2012")]
+[assembly: AssemblyCopyright("Copyright © Lyrise 2009-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
--- Library.Security/Properties/AssemblyInfo.cs
+++ Library.Security/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Library.Security")]
-[assembly: AssemblyCopyright("Copyright © Lyrise 2011-2012")]
+[assembly: AssemblyCopyright("Copyright © Lyrise 2011-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
