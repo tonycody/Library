55893704e4f2efe7619a782775769090d8b9ae69
Lyrise <al.pyfgcrl.qjkxbmwvz@gmail.com>
2013-11-03 21:42:34 +0900

    Update

--- Library.Collections/Properties/AssemblyInfo.cs
+++ Library.Collections/Properties/AssemblyInfo.cs
@@ -11,4 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("98a30c76-c619-4baf-aa08-1f2298cdeadd")]
-[assembly: AssemblyVersion("0.0.0.82")] // IYV5HWv4x2IFo1crpUNK81q5jACA24h7u0KSqqPSk3w71940MnwO1xIWB/FaGD+5G3cz+1FVVr4ZAlh5/fLOyg==
+[assembly: AssemblyVersion("0.0.0.83")] // tC4HJOFBvGrlNqf/h/vXFe/U64LGfV7KWrbdQK6WNgZPtyLUAY+BOzvGH3eGl71I4YRHUTFH6O6SrmpS0J7boQ==
--- Library.Net.Amoeba/Properties/AssemblyInfo.cs
+++ Library.Net.Amoeba/Properties/AssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8c62c763-25dc-4cf4-93fb-96b406eaf9cc")]
-[assembly: AssemblyVersion("0.0.0.2295")] // Smq9JOldkRNHOxr1uKobbBgO+Japhoo/ke4dLYNRSpAcT/EeTYbOeoH56hPvsJdHSHVqdTFhesk7lU2DOwvt3A==
+[assembly: AssemblyVersion("0.0.0.2296")] // iXDcSIzjic4UwC9xEzzuSSnZ+XplprsP29LU3Yy8rvcTWrM7NhoaQwJQBhLzT4zyxO7Wlid5evLtV8wyTddNiA==
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Library.UnitTest")]
