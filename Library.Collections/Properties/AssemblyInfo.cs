0d0047f418f6b9904887e6f9be0fbda54964a9da
Lyrise <al.pyfgcrl.qjkxbmwvz@gmail.com>
2014-06-28 14:45:24 +0900

    Fix
    微修正。

--- Library.Collections/Properties/AssemblyInfo.cs
+++ Library.Collections/Properties/AssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("98a30c76-c619-4baf-aa08-1f2298cdeadd")]
-[assembly: AssemblyVersion("0.0.0.185")] // qOX4rB4H+Pdi7FdR2IrRamtdfwkyRRnuN0r+R7ROiAHQnq3YXgaD8I/RHZmz7YY67nAdyRFaOlWoCwhukA9Bqw==
+[assembly: AssemblyVersion("0.0.0.186")] // rofjViaOuv3vqtfX6tv+Yda0toeKSELpG0ZWHMg93z67yRh7nUm9YbMxr9U+5acyRiOueQjt6BFhdckJkUeg+w==
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Library.UnitTest")]
